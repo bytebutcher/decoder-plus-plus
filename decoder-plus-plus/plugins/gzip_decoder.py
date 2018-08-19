61d49084e9319f05bab471d0f324c45ff1a6a73c
bytebutcher <thomas.engel.web@gmail.com>
2018-08-19 23:23:08 +0200

    Added gzip encode/decode plugin.

---
+++ decoder-plus-plus/plugins/gzip_decoder.py
@@ -0,0 +1,30 @@
+from core.plugin.plugin import DecoderPlugin
+
+class Plugin(DecoderPlugin):
+    """
+    Decodes bytes using Gzip.
+
+    Example:
+
+        Input:
+            [bytes]
+        Output:
+            abcdefghijklmnopqrstuvwxyz
+            ^°!"§$%&/()=?´`<>| ,.-;:_#+'*~
+            0123456789
+    """
+
+    def __init__(self, context):
+        # Name, Author, Dependencies
+        super().__init__('Gzip', "Thomas Engel", ["gzip"])
+
+    def run(self, text):
+        import gzip
+        return gzip.decompress(text.encode('utf-8', errors='surrogateescape')).decode('utf-8', errors='surrogateescape')
+
+    def can_decode_input(self, input):
+        if input:
+            input_bytes = input.encode('utf-8', errors='surrogateescape')
+            if len(input_bytes) > 2:
+                return (input_bytes[0] == 0x1f) and (input_bytes[1] == 0x8b)
+        return False
---
+++ decoder-plus-plus/plugins/gzip_encoder.py
@@ -0,0 +1,23 @@
+from core.plugin.plugin import EncoderPlugin
+
+class Plugin(EncoderPlugin):
+    """
+        Encodes a string using Gzip.
+
+        Example:
+
+            Input:
+                abcdefghijklmnopqrstuvwxyz
+                ^°!"§$%&/()=?´`<>| ,.-;:_#+'*~
+                0123456789
+            Output:
+                [bytes]
+        """
+
+    def __init__(self, context):
+        # Name, Author, Dependencies
+        super().__init__('Gzip', "Thomas Engel", ["gzip"])
+
+    def run(self, text):
+        import gzip
+        return gzip.compress(text.encode('utf-8', errors='surrogateescape')).decode('utf-8', errors='surrogateescape')
