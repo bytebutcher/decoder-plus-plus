388661505ac1cc0ed5e38bc903b82aaff9691a2d
bytebutcher <thomas.engel.web@gmail.com>
2018-09-02 21:50:32 +0200

    Added bin(str) encoder/decoder plugin.

---
+++ decoder-plus-plus/plugins/bin_str_decoder.py
@@ -0,0 +1,29 @@
+from core.plugin.plugin import DecoderPlugin
+
+class Plugin(DecoderPlugin):
+    """
+    Decodes a binary string.
+
+    Example:
+
+        Input:
+            1100001 1100010 1100011 1100100 1100101 \\
+            1100110 1100111 1101000 1101001 1101010 \\
+            ...
+            110100 110101 110110 110111 111000 111001
+        Output:
+            abcdefghijklmnopqrstuvwxyz
+            ^°!"§$%&/()=?´`<>| ,.-;:_#+'*~
+            0123456789
+    """
+
+    def __init__(self, context):
+        # Name, Author, Dependencies
+        super().__init__('BIN (str)', "Thomas Engel", [])
+
+    def safe_name(self):
+        return "bin_str"
+
+    def run(self, text):
+        n = int(text.replace(' ', ''), 2)
+        return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode("utf-8", "surrogateescape") or '\0'
---
+++ decoder-plus-plus/plugins/bin_str_encoder.py
@@ -0,0 +1,32 @@
+from core.plugin.plugin import EncoderPlugin
+
+class Plugin(EncoderPlugin):
+    """
+    Encodes a string to a binary string.
+
+    Example:
+
+        Input:
+            abcdefghijklmnopqrstuvwxyz
+            ^°!"§$%&/()=?´`<>| ,.-;:_#+'*~
+            0123456789
+        Output:
+            1100001 1100010 1100011 1100100 1100101 \\
+            1100110 1100111 1101000 1101001 1101010 \\
+            ...
+            110100 110101 110110 110111 111000 111001
+    """
+
+    def __init__(self, context):
+        # Name, Author, Dependencies
+        super().__init__('BIN (str)', "Thomas Engel", ["codecs"])
+
+    def safe_name(self):
+        return "bin_str"
+
+    def run(self, text):
+        bits = bin(int.from_bytes(text.encode('utf-8', 'surrogateescape'), 'big'))[2:]
+        return ' '.join(self._chunk_string(bits.zfill(8 * ((len(bits) + 7) // 8)), 8))
+
+    def _chunk_string(self, string, length):
+        return [string[0 + i:length + i] for i in range(0, len(string), length)]
