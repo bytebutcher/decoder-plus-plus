98f8a3d2b18e6605ab58f39a80cbb3b1645824c1
bytebutcher <thomas.engel.web@gmail.com>
2019-04-06 21:27:04 +0200

    Feature #2: Added HTTP64 encoder and decoder

---
+++ decoder-plus-plus/plugins/http64_decoder.py
@@ -0,0 +1,53 @@
+import re
+
+from core.plugin.plugin import DecoderPlugin
+
+class Plugin(DecoderPlugin):
+    """
+    Decodes a HTTP64 string.
+
+    HTTP64 is a BASE64 derivation which remaps following characters:
+
+     Location  Base64  Http64
+     --------  ------  ------
+       62        +       -
+       63        /       _
+      (pad)      =       $
+
+    Example:
+
+        Input:
+            YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXoKXsKwISLCpyQlJi8oKT0_wrRgPD58ICwuLTs6XyMrJyp-CjAxMjM0NTY3ODk$
+
+        Output:
+            abcdefghijklmnopqrstuvwxyz
+            ^°!"§$%&/()=?´`<>| ,.-;:_#+'*~
+            0123456789
+    """
+
+    def __init__(self, context):
+        # Name, Author, Dependencies
+        super().__init__('HTTP64', "Thomas Engel", ["base64"], context)
+
+    def run(self, text):
+        import base64
+        encoded = self._add_missing_padding(text.encode('utf-8', errors="surrogateescape"))
+        encoded = encoded.replace('-', '+')
+        encoded = encoded.replace('_', '/')
+        encoded = encoded.replace('$', '=')
+        return base64.b64decode(encoded).decode('utf-8', errors="surrogateescape")
+
+    def _add_missing_padding(self, text):
+        missing_padding = len(text) % 4
+        if missing_padding != 0:
+            text += '$' * (4 - missing_padding)
+        return text.decode('utf-8', errors="surrogateescape")
+
+    def can_decode_input(self, input):
+        if len(input) % 4 == 0:
+            if input.startswith("0o") and input[2:].isdigit():
+                # This looks more like an octal encoding.
+                return False
+            if re.search(r'^(?:[A-Za-z0-9\-_]{4})*(?:[A-Za-z0-9\-_]{2}\$\$|[A-Za-z0-9\-_]{3}\$)?$', input):
+                return True
+        return False
---
+++ decoder-plus-plus/plugins/http64_encoder.py
@@ -0,0 +1,37 @@
+from core.plugin.plugin import EncoderPlugin
+
+class Plugin(EncoderPlugin):
+    """
+    Encodes a text using HTTP64.
+
+    HTTP64 is a BASE64 derivation which remaps following characters:
+
+     Location  Base64  Http64
+     --------  ------  ------
+       62        +       -
+       63        /       _
+      (pad)      =       $
+
+    Example:
+
+        Input:
+            abcdefghijklmnopqrstuvwxyz
+            ^°!"§$%&/()=?´`<>| ,.-;:_#+'*~
+            0123456789
+
+        Output:
+            YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXoKXsKwISLCpyQlJi8oKT0_wrRgPD58ICwuLTs6XyMrJyp-CjAxMjM0NTY3ODk$
+    """
+
+    def __init__(self, context):
+        # Name, Author, Dependencies
+        super().__init__('HTTP64', "Thomas Engel", ["base64"], context)
+
+    def run(self, text):
+        import base64
+        encoded = base64.b64encode(text.encode('utf-8', errors="surrogateescape")).decode('utf-8', errors="surrogateescape")
+        encoded = encoded.replace('+', '-')
+        encoded = encoded.replace('/', '_')
+        encoded = encoded.replace('=', '$')
+        return encoded
+
