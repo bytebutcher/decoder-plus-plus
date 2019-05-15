77fa1d7acbab26a9dd56faf6ec22192277e9341f
bytebutcher <thomas.engel.web@gmail.com>
2019-05-15 17:17:40 +0200

    Added unescape/escape string script plugin.

---
+++ decoder-plus-plus/plugins/escape_string_script.py
@@ -0,0 +1,14 @@
+from core.plugin.plugin import ScriptPlugin
+
+
+class Plugin(ScriptPlugin):
+    """
+    Escapes the input string.
+    """
+
+    def __init__(self, context):
+        # Name, Author, Dependencies
+        super().__init__('Escape String', "Thomas Engel", [], context)
+
+    def run(self, text):
+        return repr(text)
---
+++ decoder-plus-plus/plugins/unescape_string_script.py
@@ -0,0 +1,14 @@
+from core.plugin.plugin import ScriptPlugin
+
+
+class Plugin(ScriptPlugin):
+    """
+    Unescapes the input string.
+    """
+
+    def __init__(self, context):
+        # Name, Author, Dependencies
+        super().__init__('Unescape String', "Thomas Engel", [], context)
+
+    def run(self, text):
+        return text.encode('utf-8').decode('unicode_escape')
